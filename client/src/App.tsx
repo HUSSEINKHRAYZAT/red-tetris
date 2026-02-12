90f08be9f7ab27fbb83cde8c09a4ebb35eca46c4
afayad2 <afayad2@fayad2.afayad2.virtualbox.org>
2026-02-12 17:22:13 +0200

    Router modified correctly, need test!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import MainPage from './lib/pages/MainPage'
+import GamePage from './lib/pages/GamePage'
 
-export default MainPage
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/:room/:playerName" element={<GamePage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import './index.css'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import MainPage from './lib/pages/MainPage'
-import GamePage from './lib/pages/GamePage'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/game" element={<GamePage />} />
-        <Route path="/:room/:player" element={<GamePage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
